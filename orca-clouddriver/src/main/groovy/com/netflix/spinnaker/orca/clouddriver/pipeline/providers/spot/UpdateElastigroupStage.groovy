a0fad059f3f4e76d5bfa6d932010666d06da3b47
Lasry <dor.lasry@spotinst.com>
2021-06-02 13:24:21 +0300

    [src] Add new force cache refresh task

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/pipeline/providers/spot/UpdateElastigroupStage.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/pipeline/providers/spot/UpdateElastigroupStage.groovy
@@ -21,6 +21,7 @@ import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
 import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
 import com.netflix.spinnaker.orca.clouddriver.tasks.providers.spot.UpdateElastigroupTask
+import com.netflix.spinnaker.orca.clouddriver.tasks.providers.spot.SpotServerGroupCacheForceRefreshTask
 
 import groovy.transform.CompileStatic
 import org.springframework.stereotype.Component
@@ -33,5 +34,6 @@ class UpdateElastigroupStage implements StageDefinitionBuilder {
     builder
         .withTask("updateElastigroupTask", UpdateElastigroupTask)
         .withTask("monitorUpdateElastigroup", MonitorKatoTask)
+        .withTask("forceCacheRefresh", SpotServerGroupCacheForceRefreshTask)
   }
 }
